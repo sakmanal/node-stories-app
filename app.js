d3054dc56613831f8aad395067a500490978bf25
Sakis Manal <manowarlik@gmail.com>
2020-06-25 19:40:12 +0300

    init app.js

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ const dotenv = require('dotenv')
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
